04184715919eb8da8d7b3cabb9118baef2c30d39
mateuspraxedes <mateuspr97@gmail.com>
2023-01-05 20:59:43 -0300

    iniciando o desafio

---
+++ Incomplete_Login_Form/README.md
@@ -0,0 +1,8 @@
+
+
+E aí dev, tudo bem? Trouxe pra você mais um desafio de entrevista para que você possa treinar em casa e aprimorar suas habilidades.
+
+Dessa vez temos um formulário de login incompleto e a sua missão é finalizar ele para a sua equipe.
+
+Preste bem atenção às instruções dentro do arquivo App.jsx e bora codar!
+
---
+++ Incomplete_Login_Form/src/App.jsx
@@ -0,0 +1,39 @@
+import { login } from './utils';
+import './index.css';
+import { useState } from 'react';
+
+// Instruções:
+// * Você tem um formulário de login INCOMPLETO
+// * Não é permitido adicionar novos elementos HTML
+// * Não é permitido usar refs
+//
+// Tarefas:
+// todo - O botão de login deve disparar a função login(), importada no topo deste arquivo, e passar os dados necessários.
+// todo - Desabilite o botão de Login caso o e-mail esteja em branco OU a senha for menor que 6 dígitos.
+// todo - Desabilite o botão de Login equanto você está executando o login.
+// todo - Mostre uma mensagem de erro de login() caso o Login falhe. A mensagem deve ser limpa a cada nova tentativa de Login.
+// todo - Mostre um alerta caso o login seja efetuado com sucesso (javascript alert). Investigue a função login() para entender como ter sucesso na requisição.
+
+export default function LoginForm() {
+  return (
+    <div className='wrapper'>
+      <div className='login-form'>
+        <h1>Login Form 🐞</h1>
+        {/* Coloque a mensagem de erro de login na div abaixo. Mostre a div somente se houver uma mensagem de erro. */}
+        <div className='errorMessage'></div>
+        <div className='row'>
+          <label htmlFor={'email'}>Email</label>
+          <input id={'email'} type={'email'} autoComplete='off' />
+        </div>
+        <div className='row'>
+          <label htmlFor={'password'}>Password</label>
+          <input id={'password'} type={'password'} />
+        </div>
+
+        <div className='button'>
+          <button>Login</button>
+        </div>
+      </div>
+    </div>
+  );
+}
