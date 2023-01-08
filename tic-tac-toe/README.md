a7baf1e21eafca22fe03fd5858b8b0896d357b8d
mateuspraxedes <mateuspr97@gmail.com>
2023-01-08 19:29:21 -0300

    iniciando desafio

---
+++ tic-tac-toe/README.md
@@ -0,0 +1,17 @@
+
+## React Interview Challenge #04
+
+Nosso desafio da vez é codar um JOGO DA VELHA (tic tac toe) utilizando somente react.js!
+
+Preste bem atenção às instruções dentro do arquivo App.jsx e bora codar!
+
+## Objetivos
+
+- crie um board de 3x3
+- dois jogadores
+- ao clicar em um quadrado, preencher com a jogada
+- avisar quando o jogo finalizar, caso dê velha avise também
+- fazer um risco na sequência vencedora, caso houver
+
+
+
---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+
+  // DESAFIO TÉCNICO - JOGO DA VELHA - por fernandev
+
+  // * descrição
+  //   desenvolva um jogo da velha (tic tac toe) funcional.
+  //   use qualquer técnica de estilização preferida: css modules, sass, styled.
+
+  // * tasks
+  //  todo - crie um board de 3x3
+  //  todo - dois jogadores
+  //  todo - ao clicar em um quadrado, preencher com a jogada
+  //  todo - avisar quando o jogo finalizar, caso dê velha avise também
+  //  todo - fazer um risco na sequência vencedora, caso houver
+
+
+function App() {
+  return (
+    <>
+      <div className='board-game'></div>
+
+
+    </>
+  );
+}
+
+export default App;
