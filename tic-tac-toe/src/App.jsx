7bf0f3534dab01d0545ff1cf966cc9db5478a4d1
mateuspraxedes <mateuspr97@gmail.com>
2023-01-09 17:16:49 -0300

    board 3x3 finalizado

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,14 +1,15 @@
 import './App.css';
 
 
-  // DESAFIO TÉCNICO - JOGO DA VELHA - por fernandev
+
+  // DESAFIO TÉCNICO - JOGO DA VELHA 
 
   // * descrição
   //   desenvolva um jogo da velha (tic tac toe) funcional.
   //   use qualquer técnica de estilização preferida: css modules, sass, styled.
 
   // * tasks
-  //  todo - crie um board de 3x3
+  //  * todo - crie um board de 3x3
   //  todo - dois jogadores
   //  todo - ao clicar em um quadrado, preencher com a jogada
   //  todo - avisar quando o jogo finalizar, caso dê velha avise também
@@ -18,8 +19,29 @@ import './App.css';
 function App() {
   return (
     <>
-      <div className='board-game'></div>
+      <div className='board-game'>
+        <div className='board-game__row'>
+          <div className='board-game__cell'>❌</div>
+          <div className='board-game__cell '>⭕️</div>
+          <div className='board-game__cell'>❌</div>
 
+      </div>
+
+      <div className='board-game__row'>
+          <div className='board-game__cell'>❌</div>
+          <div className='board-game__cell '>⭕️</div>
+          <div className='board-game__cell'>❌</div>
+
+      </div>
+
+      <div className='board-game__row'>
+          <div className='board-game__cell'>❌</div>
+          <div className='board-game__cell '>⭕️</div>
+          <div className='board-game__cell'>❌</div>
+
+      </div>
+
+      </div>
 
     </>
   );
